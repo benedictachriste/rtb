82106576f9babe842af69a20fc2eead73760f894
benedictachriste <benedictachris3@gmail.com>
2024-03-27 00:03:31 +0700

    popup

---
+++ bookingmain.js
@@ -0,0 +1,14 @@
+// document.getElementById("openPopup").addEventListener("click", function(){
+    
+// });
+// document.getElementById("closePopup").addEventListener("click", function(){
+//     document.getElementById("overlay").style.display="none";
+// });
+
+function popup() {
+    document.getElementById("overlay").style.display="block";
+}
+
+function closepopup(){
+    document.getElementById("overlay").style.display="none";
+}
