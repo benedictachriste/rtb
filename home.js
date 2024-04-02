8599577f21209d6301cb89852d60536801093a4a
jejejessiee <wenjess2124@gmail.com>
2024-04-02 16:15:16 +0700

    animasi homepage + icon home di login page ku a tag ke np-homepage

---
+++ home.js
@@ -0,0 +1,10 @@
+const image = ["/img/building.png", "/img/building2.png", "/img/building3.png"]
+const imageDiv = document.querySelector(".wrapper")
+let currIdx = 0
+
+function changeImage() {
+    currIdx = (currIdx + 1) % image.length;
+    imageDiv.style.backgroundImage = `url("${image[currIdx]}")`;
+}
+
+setInterval(changeImage, 3000)
