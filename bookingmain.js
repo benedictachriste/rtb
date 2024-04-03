71ee8071090eeb7e84ad962b64f19c5b2401a30a
benedictachriste <benedictachris3@gmail.com>
2024-04-03 10:41:28 +0700

    booking main done

--- bookingmain.js
+++ bookingmain.js
@@ -64,11 +64,11 @@ dropArea.addEventListener("drop", function(e){
 
 function toggleSlideMenu() {
   var slideMenu = document.getElementById('slideMenu');
-  if (slideMenu.style.right === "-400px") {
+  if (slideMenu.style.right === "-25vw") {
       slideMenu.style.right = "0";
       // document.querySelector("body").classList.add("body-no-scroll")
     } else {
-      slideMenu.style.right = "-400px";
+      slideMenu.style.right = "-25vw";
       // document.querySelector("body").classList.remove("body-no-scroll")
   }
 }
---
+++ np-home.js
@@ -0,0 +1,21 @@
+function toggleSlideMenu() {
+    var slideMenu = document.getElementById('slideMenu');
+    if (slideMenu.style.right === "-25vw") {
+        slideMenu.style.right = "0";
+        // document.querySelector("body").classList.add("body-no-scroll")
+      } else {
+        slideMenu.style.right = "-25vw";
+        // document.querySelector("body").classList.remove("body-no-scroll")
+    }
+  }
+
+const image = ["/img/building.png", "/img/building2.png", "/img/building3.png"]
+const imageDiv = document.querySelector(".wrapper")
+let currIdx = 0
+
+function changeImage() {
+    currIdx = (currIdx + 1) % image.length;
+    imageDiv.style.backgroundImage = `url("${image[currIdx]}")`;
+}
+
+setInterval(changeImage, 3000)
