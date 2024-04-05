38ffeada5d02456c48faab9b8ec0d7be2f54d2ca
YupiBear <juwitanethaniachandra@gmail.com>
2024-04-05 13:45:58 +0700

    laundry rules

--- notif.js
+++ notif.js
@@ -4,6 +4,6 @@ notifIcon.addEventListener("click", function() {
   if (slideMenu.style.right != "0px") {
     slideMenu.style.right = "0"
   } else {
-    slideMenu.style.right = "-30gvw"
+    slideMenu.style.right = "-30vw"
   }
 })
