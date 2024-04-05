9f51713b3663ba57486631ae172e8cb8043400e3
benedictachriste <benedictachris3@gmail.com>
2024-04-05 10:40:20 +0700

    notif shadow

--- notif.js
+++ notif.js
@@ -4,6 +4,6 @@ notifIcon.addEventListener("click", function() {
   if (slideMenu.style.right != "0px") {
     slideMenu.style.right = "0"
   } else {
-    slideMenu.style.right = "-25vw"
+    slideMenu.style.right = "-30gvw"
   }
 })
